ba356bd86dd84de93cb9ded97e086c0b63fbc24d
Augusto <augustojedamathe@gmail.com>
2024-08-31 16:12:58 -0300

    creo la pieza t

---
+++ src/test/java/com/example/tetrisgame/PieceBaseTest.java
@@ -0,0 +1,10 @@
+package com.example.tetrisgame;
+import org.junit.jupiter.api.Test;
+
+public class PieceBaseTest {
+    @Test
+    public void crearPieceBase() {
+        PieceBase pb = new PieceBase();
+        assert pb != null;
+    }
+}
--- src/test/java/com/example/tetrisgame/PieceTTest.java
+++ src/test/java/com/example/tetrisgame/PieceTTest.java
@@ -1,5 +1,10 @@
 package com.example.tetrisgame;
+import org.junit.jupiter.api.Test;
 
 public class PieceTTest {
-    
+    @Test
+    public void crearPieceT() {
+        PieceT p = new PieceT();
+        assert p != null;
+    }
 }
