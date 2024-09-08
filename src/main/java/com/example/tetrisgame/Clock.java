b2adbd9d4e6c96e1d9a194b245b35740f6d1f6ed
Joaco981 <joaquinnfloress16@gmail.com>
2024-09-08 18:53:57 -0300

    metodos get y set para la clase Clock

--- src/main/java/com/example/tetrisgame/Clock.java
+++ src/main/java/com/example/tetrisgame/Clock.java
@@ -1,5 +1,17 @@
 package com.example.tetrisgame;
 
 public class Clock {
-    
+    private int tic = 0;
+
+
+    public int getTick(){
+        return tic;
+    }
+
+    public void setTick(int valor){
+        valor = tic;
+    }
+
+
+
 }
