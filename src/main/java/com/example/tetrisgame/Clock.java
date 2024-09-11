8233d2b2f7e4521c33dedc9f0e5afa578c3154aa
Joaco981 <joaquinnfloress16@gmail.com>
2024-09-11 11:33:49 -0300

    se testea bajar la pieza con tic()

--- src/main/java/com/example/tetrisgame/Clock.java
+++ src/main/java/com/example/tetrisgame/Clock.java
@@ -2,14 +2,20 @@ package com.example.tetrisgame;
 
 public class Clock {
     private int ticCont = 0;
+    private Board board;
 
+    public Clock(Board board) {
+        this.board = board;
+    }
 
-    public int getTick(){
+    public int getTick() {
         return ticCont;
     }
 
-    public void tic(){
+    public void tic() {
         ticCont++;
+        // Llamar al método bajarPieza del tablero
+        board.bajarPieza(board.piezaActual);
     }
-
 }
+
--- src/test/java/com/example/tetrisgame/ClockTest.java
+++ src/test/java/com/example/tetrisgame/ClockTest.java
@@ -10,11 +10,16 @@ public class ClockTest {
     @Test
     // 1 segundo por tic
     void prueba_contador_test(){
-        Clock c1 = new Clock();
+        Board b1 = new Board();
+        Clock c1 = new Clock(b1);
 
         c1.tic();
 
         assertEquals(1, c1.getTick());
+        // Recorrer toda la primera fila y verificar que no hay ningún '*'
+        for (int j = 0; j < b1.getBoard()[0].length; j++) {
+            assert b1.getBoard()[0][j] == '.';
+        }
 
     }
     
