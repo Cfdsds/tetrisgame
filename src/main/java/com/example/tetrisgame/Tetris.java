dde783231d71ca65af6d10ce7c825f1f9149eb9f
Augusto <augustojedamathe@gmail.com>
2024-09-15 10:08:12 -0300

    agregue el metodo llegarAlFinal y empece a modificar la clase Tetris

--- src/main/java/com/example/tetrisgame/Tetris.java
+++ src/main/java/com/example/tetrisgame/Tetris.java
@@ -1,5 +1,18 @@
 package com.example.tetrisgame;
 
-public class Tetris {
-    //por ahora nada
+public class Tetris extends Clock {
+    public Board board;
+
+    public Tetris() {
+        board = new Board();
+        Clock c1 = new Clock();
+    }
+
+    public void ganar() {
+        //
+    }
+
+    public void perder() {
+        //
+    }
 }
